f2e163188b3dbe7ffb84d85ac86462c6a5db6c4d
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2024-01-01 17:17:54 +0000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,17 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://mongo:27017/')
 db = client['bookstore']
 collection = db['books']
+
+@app.route('/', methods=['GET'])
+def index():
+   return "Hello, I am Online!!!"
 
 @app.route('/books', methods=['GET'])
 def get_all_books():
    books = collection.find()
    return jsonify(list(books))
 
-# Additional routes for POST, PUT, DELETE methods go here
+@app.route('/add-book', methods=['POST'])
+def add_book():
+   book = request.get_json()
+   collection.insert_one(book)
+   return jsonify(book), 201
 
 if __name__ == '__main__':
    app.run(debug=True)
